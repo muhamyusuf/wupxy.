2e65aa385a9bf1e6e4a6aa0974bffe8bf2848acf
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-07-04 13:45:03 +0700

    fix bug frammer-motion

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,5 @@
-import { motion } from 'framer-motion';
-import Navbar from '@/components/Navbar';
-import { usePathname } from 'next/navigation';
-
 import './globals.css';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
   title: { default: 'Wupxy', template: '%s | Wupxy' },
@@ -14,36 +11,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const pathName = usePathname();
-
   return (
     <html lang="en">
       <body>
         <div className="container relative flex items-center">
           <Navbar />
-          <motion.div
-            key={pathName}
-            initial="initialState"
-            animate="animateState"
-            exit="exitState"
-            transition={{ duration: 0.3, ease: 'easeInOut' }}
-            variants={{
-              initialState: {
-                opacity: 0,
-                transform: 'translateY(5%)',
-              },
-              animateState: {
-                opacity: 1,
-                transform: 'translateY(0%)',
-              },
-              exitState: {
-                opacity: 0,
-              },
-            }}
-            className="px-4 py-10 tablet:py-30 laptop:py-40 tablet:px-8 ml-0 tablet:ml-[200px] laptop:ml-[320px]"
-          >
+
+          <div className="px-4 py-10 tablet:py-30 laptop:py-40 tablet:px-8 ml-0 tablet:ml-[200px] laptop:ml-[320px]">
             {children}
-          </motion.div>
+          </div>
         </div>
       </body>
     </html>
