68a3df092a3c0df7189c01f1f21c8bcc9ec6758c
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-07-05 14:08:30 +0700

    edit missunderstand of /app layout for vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import './globals.css';
 import Navbar from '@/components/Navbar';
-import { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
@@ -10,22 +9,20 @@ export const metadata = {
 
 export default function RootLayout({
   children,
-  pageProps,
 }: {
-  pageProps: AppProps;
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <div className="container relative flex items-center" {...pageProps}>
+        <div className="container relative flex items-center">
           <Navbar />
 
           <div className="px-4 py-10 tablet:py-30 laptop:py-40 tablet:px-8 ml-0 tablet:ml-[200px] laptop:ml-[320px]">
             {children}
           </div>
-          <Analytics />
         </div>
+        <Analytics />
       </body>
     </html>
   );
