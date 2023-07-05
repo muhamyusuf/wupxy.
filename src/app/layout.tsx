643b8bda9028410666ac7653389faa380e0cd4b5
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-07-05 14:01:07 +0700

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata = {
   title: { default: 'Wupxy', template: '%s | Wupxy' },
@@ -8,18 +10,21 @@ export const metadata = {
 
 export default function RootLayout({
   children,
+  pageProps,
 }: {
+  pageProps: AppProps;
   children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <div className="container relative flex items-center">
+        <div className="container relative flex items-center" {...pageProps}>
           <Navbar />
 
           <div className="px-4 py-10 tablet:py-30 laptop:py-40 tablet:px-8 ml-0 tablet:ml-[200px] laptop:ml-[320px]">
             {children}
           </div>
+          <Analytics />
         </div>
       </body>
     </html>
