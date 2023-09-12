89bb9c1b162fb885910683786497154f211ca50d
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-09-12 10:50:29 +0700

    package:add tailwind-scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="scrollbar-none">
         <div className="container relative flex items-center">
           <Navbar />
 
